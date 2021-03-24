ccd1f2a0cca54b4224e6e10959ac4e291ebd5353
dikshapg <dikshapg1998@gmail.com>
2021-03-24 17:12:53 +0530

    Altered Abc.java and Constructed Xyz.java

--- Abc.java
+++ Abc.java
@@ -3,5 +3,9 @@ public class Abc
 	public static void main(String args[])
 	{
 		System.out.println("HELLO");
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("Hi");	
+		}
 	}
 }
---
+++ Xyz.java
@@ -0,0 +1,18 @@
+public class Xyz
+{
+	private int x;
+	private String str;
+	public void setStr()
+	{
+	}
+	public void setX()
+	{
+	}
+	
+	public void getX()
+	{
+		return x;
+	}
+	
+	
+}
